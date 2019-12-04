7f5df203d8edd0e57449c8a4898402a4bf95796f
Will Ness <willness210@gmail.com>
2019-12-04 14:58:40 -0700

    removed extra

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import time
-=======
-#Calculate final position function
-def calcBallFinalPosition(input):
->>>>>>> af29416f625d4b84a7c3a1bc63374722653f1f33
 
 #IK funtion for arm
 def invK():
 
-
-
-
-<<<<<<< HEAD
 #Store ball location history
 pos_hist = [[], [], [], [], [], [], []]
 def addSensors(aX, aY, aZ, bX, bY, bZ):
@@ -22,11 +13,6 @@ def addSensors(aX, aY, aZ, bX, bY, bZ):
     pos_hist[4].append(bX)
     pos_hist[5].append(bY)
     pos_hist[6].append(bZ)
-=======
-#IK funtion for arm
-def calcArmPosition():
-
->>>>>>> af29416f625d4b84a7c3a1bc63374722653f1f33
 
 #Calculate final position function
 def calcPosition():
